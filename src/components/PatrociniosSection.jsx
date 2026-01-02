361cf86327bf9d1e7071ca5765926ab4331866c7
Andrespeerez <andreslavila93@gmail.com>
2026-01-02 16:46:43 +0100

    chore: add some links to sponsonrs

--- src/components/PatrociniosSection.jsx
+++ src/components/PatrociniosSection.jsx
@@ -5,13 +5,13 @@ export default function PatrociniosSection () {
         {img: "/patrocinadores/1_escuder_letras.webp", title: "Textiles Escuder", url: "https://www.vicenteescuder.com/"},
         {img: "/patrocinadores/2_logocompleto1sinfondo.webp", title: "Adenasim perifericos simracing", url: "https://www.adrenasim.com/"},
         {img: "/patrocinadores/3_Logo_Juan_Guardiola_2.webp", title: "Koses online - cosas antiguas", url: "https://kosasonline.com/"},
-        {img: "/patrocinadores/4_ar_blanco.webp", title: "AR photo racing"},
-        {img: "/patrocinadores/5_joval_logo__letras_hor.webp", title: "joval"},
+        {img: "/patrocinadores/4_ar_blanco.webp", title: "AR photo racing", url: "https://www.instagram.com/arphotoracing/"},
+        {img: "/patrocinadores/5_joval_logo__letras_hor.webp", title: "joval", url: "https://www.joval.es/"},
         {img: "/patrocinadores/6_Totalitym-Blanco-1024x155.webp", title: "totalitym - camisetas e-sports"},
         {img: "/patrocinadores/7_logo-instant-gaming.webp", title: "Instant Gaming - Juegos al Mejor precio", url: "https://www.instant-gaming.com/?igr=afondusimracing"},
-        {img: "/patrocinadores/8_DG.webp", title: "diseño gráfico DG"},
-        {img: "/patrocinadores/9_Logo_Piston_Arts_5.webp", title: "diseño gráfico Liston Arts"},
-        {img: "/patrocinadores/10_GT2i_Officiel.webp", title: "GT2i - tienda motor sport"},
+        {img: "/patrocinadores/8_DG.webp", title: "diseño gráfico DG", url: "https://www.instagram.com/dg.sign/"},
+        {img: "/patrocinadores/9_Logo_Piston_Arts_5.webp", title: "diseño gráfico Liston Arts", url: "https://www.instagram.com/pistonarts_designs/"},
+        {img: "/patrocinadores/10_GT2i_Officiel.webp", title: "GT2i - tienda motor sport", url: "https://www.gt2i.es/es/"},
         {img: "/patrocinadores/11_Logo_Siller_Madridderally.webp", title: "Canal de Siller", url: "https://www.youtube.com/@madridderally"},
     ];
 
